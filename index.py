b33b0796fce728df2f74db079a5d4d0c34f44ac3
Quentin Metivier <q.metivier@iia-laval.fr>
2026-02-28 00:36:05 +0100

    upgrade readability

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ import json
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix="Q ", intents=discord.Intents.default())
+bot = commands.Bot(command_prefix="p ", intents=discord.Intents.default())
 channel_name = os.getenv("CHANNEL")
 token = os.getenv("TOKEN")
 app_token = os.getenv("APP_TOKEN")
@@ -31,7 +31,17 @@ def get_right_channels():
     for channel in bot.get_all_channels():
         if channel.name == channel_name:
             channels.append(channel)
+
     return channels
+
+
+def is_within_30min(date_debut: str):
+    date_debut = datetime.fromisoformat(date_debut.replace("Z", "+00:00"))
+    paris_tz = zoneinfo.ZoneInfo("Europe/Paris")
+    now = datetime.now(paris_tz)
+    diff = now - date_debut
+
+    return 30 * 60 > abs(diff.total_seconds()) > 25 * 60
 
 
 @bot.event
@@ -48,17 +58,14 @@ async def notif_anomalie():
     request = requests.request("GET", anomalie_url, headers=headers)
     if request.status_code != 200:
         return
-    next_spawn_time = request.json()['nextSpawnTime']
-    date = datetime.fromisoformat(next_spawn_time.replace("Z", "+00:00"))
-    paris_tz = zoneinfo.ZoneInfo("Europe/Paris")
-    now = datetime.now(paris_tz)
-    diff = now - date
-    is_within_30min = 30 * 60 > abs(diff.total_seconds()) > 25 * 60
+    if 'nextSpawnTime' not in request.json():
+        return
 
-    if is_within_30min:
+    next_spawn_time = request.json()['nextSpawnTime']
+    if is_within_30min(next_spawn_time):
         for channel in channels:
             await bot.get_channel(channel.id).send(
-                "Une anomalie arrive dans moins de 30 minutes ! Chargez !!! Dans quelques minutes..."
+                "<@&1476885924767076485> Une anomalie arrive dans moins de 30 minutes ! Chargez !!! Dans quelques minutes..."
             )
 
 
@@ -68,17 +75,16 @@ async def notif_derby():
     request = requests.request("GET", derby_url, headers=headers)
     if request.status_code != 200:
         return
-    derby_infos_upcoming = request.json()['upcoming'][0]
-    date_debut = datetime.fromisoformat(derby_infos_upcoming['start_time'].replace("Z", "+00:00"))
-    paris_tz = zoneinfo.ZoneInfo("Europe/Paris")
-    now = datetime.now(paris_tz)
-    diff = now - date_debut
-    is_within_30min = 30 * 60 > abs(diff.total_seconds()) > 25 * 60
 
-    if is_within_30min:
+    derby_infos_upcoming = request.json()['upcoming'][0]
+
+    date_debut = derby_infos_upcoming['start_time']
+    if is_within_30min(date_debut):
         for channel in channels:
             await bot.get_channel(channel.id).send(
-                "Un derby commence dans moins de 30 minutes biome " + derby_infos_upcoming['biome_id'] + " ! Chargez !!! Dans quelques minutes..."
+                "<@&1476885924767076485> Un derby commence dans moins de 30 minutes biome "
+                + str(derby_infos_upcoming['biome_id'])
+                + " ! Chargez !!! Dans quelques minutes..."
             )
 
 
@@ -88,17 +94,16 @@ async def notif_tournament():
     request = requests.request("GET", tournament_url, headers=headers)
     if request.status_code != 200:
         return
-    tournament_infos_upcoming = request.json()['upcoming'][0]
-    date_debut = datetime.fromisoformat(tournament_infos_upcoming['start_time'].replace("Z", "+00:00"))
-    paris_tz = zoneinfo.ZoneInfo("Europe/Paris")
-    now = datetime.now(paris_tz)
-    diff = now - date_debut
-    is_within_30min = 30 * 60 > abs(diff.total_seconds()) > 25 * 60
 
-    if is_within_30min:
+    tournament_infos_upcoming = request.json()['upcoming'][0]
+
+    date_debut = tournament_infos_upcoming['start_time']
+    if is_within_30min(date_debut):
         for channel in channels:
             await bot.get_channel(channel.id).send(
-                "Un tournoie commence dans moins de 30 minutes biome " + tournament_infos_upcoming['biome_id'] + " ! Chargez !!! Dans quelques minutes..."
+                "<@&1476885924767076485> Un tournoie commence dans moins de 30 minutes biome "
+                + str(tournament_infos_upcoming['biome_id'])
+                + " ! Chargez !!! Dans quelques minutes..."
             )
 
 bot.run(token)
